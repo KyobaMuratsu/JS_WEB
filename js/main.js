d171eb628c25d9a47faf8c32e6b95d1b1f10f1cd
Bruno Bernado Tremarin <bruno-tremarin@hotmail.com>
2023-07-09 20:45:22 -0300

    Construção do codigo: Criar um novo item na lista

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
+function show(log) {
+    console.log(log);
+}
+
+const lista = document.getElementById("lista");
 const formulario = document.getElementById("novoItem");
 
 formulario.addEventListener("submit", (evento) => {
     evento.preventDefault();
-    evento.target[elements['nome']].value;
+
+    criarItem(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value);
 });
+
+function criarItem(nome, quantidade) {
+    show(nome);
+    show(quantidade);
+
+    const item = document.createElement('li');
+    item.classList.add("item");
+    
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = quantidade;
+
+    item.appendChild(numeroItem);
+    item.innerHTML += nome;
+
+    lista.appendChild(item);
+
+    show(item)
+}
