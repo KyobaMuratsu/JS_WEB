13c61f27a799a394334f77ffa1a13acd6c593e25
Bruno Bernado Tremarin <bruno-tremarin@hotmail.com>
2023-07-09 19:33:59 -0300

    Primeiras atualizações

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const formulario = document.getElementById("novoItem");
+
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    evento.target[elements['nome']].value;
+});
