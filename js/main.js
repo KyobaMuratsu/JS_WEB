01d69bd4a63eda38a7863b069e04140cfe26c612
Bruno Bernado Tremarin <bruno-tremarin@hotmail.com>
2023-07-11 15:43:38 -0300

    Projeto Finalizado

--- js/main.js
+++ js/main.js
@@ -16,14 +16,27 @@ formulario.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade'];
 
+    const existe = itens.find( item  => item.nome === nome.value);
+
     const itematual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
 
-    criarItem(itematual);
+    if(existe){
+        itematual.id = existe.id;
 
-    itens.push(itematual);
+        atualizarItem(itematual);
+
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itematual
+    }else{
+        itematual.id = itens[itens.length - 1] ? (itens[itens.length-1]).id + 1 : 0;
+
+        criarItem(itematual);
+
+        itens.push(itematual);
+
+    }
 
     localStorage.setItem("itens", JSON.stringify(itens));
 
@@ -38,10 +51,35 @@ function criarItem(item) {
     
     const numeroItem = document.createElement('strong');
     numeroItem.innerHTML = item.quantidade;
+    numeroItem.dataset.id = item.id;
     novoitem.appendChild(numeroItem);
 
     novoitem.innerHTML += item.nome;
 
+    novoitem.appendChild(deletarItem(item.id));
+
     lista.appendChild(novoitem);
 
+}
+
+function atualizarItem(item) {
+    document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade;
+}
+
+function deletarItem(id) {
+    const botao = document.createElement("button")
+    botao.innerText = "X";
+
+    botao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id);
+    });
+    return botao;
+}
+
+function deletaElemento(tag, id) {
+    tag.remove();
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+
+    localStorage.setItem("itens", JSON.stringify(itens));
 }
